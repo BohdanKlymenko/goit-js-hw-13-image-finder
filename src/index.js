bfa66afb8a6b20a1d58ea7e98a5260397c208e13
BohdanKlymenko <81781697+BohdanKlymenko@users.noreply.github.com>
2021-10-03 07:20:40 +0300

    добавляет основной функционал

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+import 'material-icons/iconfont/material-icons.css';
 import './sass/main.scss';
+
+import refs from './js/refs';
+import ApiService from './js/apiService';
+import BtnService from './js/btnService';
+
+import photoCard from './template/photo-card.hbs';
+
+const { formSearch, imgGallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
+
+const imageService = new ApiService();
+const button = new BtnService({
+  loadMoreBtn: loadMoreBtn,
+  loadMoreLabel: loadMoreLabel,
+  loadMoreSpinner: loadMoreSpinner,
+  classList: 'd-none',
+});
+
+const fetchImg = event => {
+  event.preventDefault();
+
+  const userRequest = event.currentTarget.elements.query.value.trim();
+  imageService.query = userRequest;
+  imageService.fetchImg().then(hits => {
+    imgGallery.insertAdjacentHTML('beforeend', photoCard(hits));
+  });
+  formSearch.reset();
+};
+
+formSearch.addEventListener('submit', fetchImg);
---
+++ src/js/apiService.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const apiKey = '23672308-52491526fc628ac9f579eb3fa';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 12;
+  }
+
+  async fetchImg() {
+    try {
+      let response = await axios.get(
+        `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=${this.perPage}&key=${apiKey}`,
+      );
+      let data = await response?.data?.hits;
+      return data;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery
+  }
+
+  set query(value) {
+    this.searchQuery = value
+  }
+}
